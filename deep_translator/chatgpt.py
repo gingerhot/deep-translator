ea556144484b165c1bf0cd792947343bbacedab6
B1nj0y <idegorepl@gmail.com>
2025-04-15 10:00:41 +0800

    Update OpenAI API

--- deep_translator/chatgpt.py
+++ deep_translator/chatgpt.py
@@ -1,11 +1,15 @@
 __copyright__ = "Copyright (C) 2020 Nidhal Baccouri"
 
-import os
 from typing import List, Optional
 
 from deep_translator.base import BaseTranslator
-from deep_translator.constants import OPEN_AI_ENV_VAR
+from deep_translator.constants import (
+    OPENAI_API_KEY,
+    OPENAI_BASE_URL,
+    OPENAI_MODEL,
+)
 from deep_translator.exceptions import ApiKeyException
+from dotenv import dotenv_values
 
 
 class ChatGptTranslator(BaseTranslator):
@@ -13,24 +17,29 @@ class ChatGptTranslator(BaseTranslator):
     class that wraps functions, which use the ChatGPT
     under the hood to translate word(s)
     """
+    ENV_VARS = dotenv_values(".env")
 
     def __init__(
         self,
         source: str = "auto",
         target: str = "english",
-        api_key: Optional[str] = os.getenv(OPEN_AI_ENV_VAR, None),
-        model: Optional[str] = "gpt-3.5-turbo",
+        api_key: Optional[str] = ENV_VARS.get(OPENAI_API_KEY, None),
+        base_url: Optional[str] = ENV_VARS.get(OPENAI_BASE_URL, None),
+        model: Optional[str] = ENV_VARS.get(OPENAI_MODEL, "gpt-3.5-turbo"),
         **kwargs,
     ):
         """
         @param api_key: your openai api key.
+        @param base_url: your openai base URL.
+        @param model: which model you choose.
         @param source: source language
         @param target: target language
         """
         if not api_key:
-            raise ApiKeyException(env_var=OPEN_AI_ENV_VAR)
+            raise ApiKeyException(env_var=OPENAI_API_KEY)
 
         self.api_key = api_key
+        self.base_url = base_url
         self.model = model
 
         super().__init__(source=source, target=target, **kwargs)
@@ -40,14 +49,16 @@ class ChatGptTranslator(BaseTranslator):
         @param text: text to translate
         @return: translated text
         """
-        import openai
+        from openai import OpenAI
 
-        openai.api_key = self.api_key
+        client = OpenAI(
+            api_key=self.api_key,
+            base_url=self.base_url if self.base_url else None
+        )
 
-        prompt = f"Translate the text below into {self.target}.\n"
-        prompt += f'Text: "{text}"'
+        prompt = "Translate below into {}, only return translated result:\n{}".format(self.target, text)
 
-        response = openai.ChatCompletion.create(
+        response = client.chat.completions.create(
             model=self.model,
             messages=[
                 {
